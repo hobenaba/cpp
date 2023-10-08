7c550b23ebff02a7406acfeeb364647b87ba6481
Houda Benabad <hobenaba@e1r3p3.1337.ma>
2023-10-08 18:53:21 +0100

    done

---
+++ account.cpp
@@ -0,0 +1,34 @@
+#include "Account.hpp"
+#include <iostream>
+
+void	Account::makeDeposit(int deposit)
+{
+    return;
+}
+bool	Account::makeWithdrawal(int withdrawal)
+{
+    return (1);
+}
+int		Account::checkAmount(void) const
+{
+    std::cout << _amount << std::endl;
+    return (10);
+}
+void	Account::displayStatus(void) const
+{
+    return ;
+}
+Account::Account(int initial_deposit)
+{
+    //std::cout << "amount:" << initial_deposit;
+    return;
+}
+Account::~Account(void)
+{
+    return ;
+}
+void	Account::displayAccountsInfos(void)
+{
+    std::cout << "imhier\n" << std::endl;
+    return;
+}
