6916d0f2b9cbf266a1555857439aa59018ee535a
Houda Benabad <hobenaba@e2r4p4.1337.ma>
2023-10-11 17:21:35 +0100

    done

--- main.cpp
+++ main.cpp
@@ -6,21 +6,21 @@
 /*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 16:04:37 by hobenaba          #+#    #+#             */
-/*   Updated: 2023/10/11 16:32:03 by hobenaba         ###   ########.fr       */
+/*   Updated: 2023/10/11 17:18:32 by hobenaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Weapon.hpp"
+#include "HumanA.hpp"
 
 int main()
 {
-    // {
-    //     Weapon club = Weapon("crude spiked club");
-    //     HumanA bob("Bob", club);
-    //     bob.attack();
-    //     club.setType("some other type of club");
-    //     bob.attack();
-    // }
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanA bob("Bob", club);
+        // bob.attack();
+        // club.setType("some other type of club");
+        // bob.attack();
+    }
     // {
     //     Weapon club = Weapon("crude spiked club");
     //     HumanB jim("Jim");
