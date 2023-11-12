f4eab0c381e32f48b9475977e99dd0fb31fd0cd0
Houda Benabad <hobenaba1337@gmail.com>
2023-11-12 14:48:00 +0100

    Done

--- Brain.cpp
+++ Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 15:25:12 by hobenaba          #+#    #+#             */
-/*   Updated: 2023/11/08 15:27:13 by hobenaba         ###   ########.fr       */
+/*   Updated: 2023/11/08 15:31:38 by hobenaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,12 @@ Brain::~Brain()
 {
     std::cout << "Brain is destroyed" << std::endl;
 }
-Brain::Brain(const Brain &src) : Bra
-Brain Brain::operator=(const Brain &src);
+Brain::Brain(const Brain &src)
+{
+    std::cout << "Brain created" << std::endl;
+   
+}
+Brain Brain::operator=(const Brain &src)
+{
+    
+}
