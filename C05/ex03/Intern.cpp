5e1f60ac7fb43c051aedf6e617e46b584cee2bae
Houda Benabad <hobenaba1337@gmail.com>
2023-11-27 17:19:11 +0100

    done

--- C05/ex03/Intern.cpp
+++ C05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 18:31:40 by hobenaba          #+#    #+#             */
-/*   Updated: 2023/11/26 19:01:27 by hobenaba         ###   ########.fr       */
+/*   Updated: 2023/11/27 17:16:02 by hobenaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,28 +39,14 @@ Intern &Intern::operator=(const Intern &src)
 Form *Intern::makeForm(std::string form, std::string target)
 {
     std::string str[3] = {"robotomy request", "presidential pardon", "shrubbery creation"};
+    Form *f[3] = {new RobotomyRequestForm(target), new PresidentialPardonForm(target), new ShrubberyCreationForm(target)};
     int i = -1;
-    Form *f;
     
     while (++i < 3)
     {
         if (str[i] == form)
-        {
-            switch(i)
-            {
-                case 0:
-                    f = new RobotomyRequestForm(target);
-                    break;
-                case 1:
-                    f = new PresidentialPardonForm(target);
-                    break;
-                case 2:
-                    f = new ShrubberyCreationForm(target);
-                    break;
-            }
             return (std::cout << "intern creates " << form
-                << std::endl, f);
-        }
+                << std::endl, f[i]);
     }
     std::cout << "this form doesn't exit" << std::endl;
     return (NULL);
