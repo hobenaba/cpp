815327d77dbf1d0118f19b5fe4c9d034d69c8f6c
Houda Benabad <hobenaba1337@gmail.com>
2023-12-13 15:43:28 +0100

    done

---
+++ C07/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 15:29:39 by hobenaba          #+#    #+#             */
+/*   Updated: 2023/12/13 15:38:39 by hobenaba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void )
+{
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
---
+++ C07/ex00/whatever.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 15:30:18 by hobenaba          #+#    #+#             */
+/*   Updated: 2023/12/13 15:42:48 by hobenaba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+
+void swap(T &x, T &y)
+{
+    T tmp;
+
+    tmp = x;
+    x = y;
+    y = tmp;
+}
+
+template <typename T>
+
+//do we have the right to use teranry operators.
+T min(T &x, T &y)
+{
+    return ((x < y) ? x : y);
+}
+
+template <typename T >
+
+T max(T &x, T &y)
+{
+    return ((x > y) ? x : y);
+}
