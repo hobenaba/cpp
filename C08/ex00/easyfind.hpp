a882a391a304e955455be99b532b800820f39e29
Houda Benabad <hobenaba1337@gmail.com>
2023-12-20 21:10:35 +0100

    done

--- C08/ex00/easyfind.hpp
+++ C08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 19:12:43 by hobenaba          #+#    #+#             */
-/*   Updated: 2023/12/19 19:37:44 by hobenaba         ###   ########.fr       */
+/*   Updated: 2023/12/20 21:06:31 by hobenaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,11 @@
 
 template <typename T>
 
-typename T::iterator easyfind(T container, int toFind)
+typename T::iterator easyfind(T &container, int toFind)
 {
     typename T::iterator it = std::find(container.begin(), container.end(), toFind);
     
-    try{
-        if (it == container.end())
-            throw "value not Found";
-        std::cout << "value Found" << std::endl;
-    }
-    catch (const char *str)
-    {
-        std::cout << str << std::endl;
-    }
+    if (it == container.end())
+        throw std::runtime_error("value not found");
     return (it);
 }
--- C08/ex00/main.cpp
+++ C08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 19:17:22 by hobenaba          #+#    #+#             */
-/*   Updated: 2023/12/20 19:39:07 by hobenaba         ###   ########.fr       */
+/*   Updated: 2023/12/20 21:02:39 by hobenaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,27 @@
 
 int main ()
 {
-    std::vector<int> numbers;
-    std::vector<int>::iterator it;
-    int i = 0;
+    try 
+    {
 
-    while (++i < 10)
+        std::vector<int> numbers;
+        std::vector<int>::iterator it;
+        int i = -1;
+
+        while (++i < 10)
         numbers.push_back(i * i);
-    
-    it = numbers.begin() - 1;
-    while (++it < numbers.end())
-        std::cout << *it << std::endl;
 
-    easyfind(numbers, 1);
-    easyfind(numbers, 0);
-    easyfind(numbers, -11);
+        it = numbers.begin() - 1;
+        while (++it < numbers.end())
+        std::cout << *it << std::endl;
+        
+        std::cout << "========" << std::endl;
+        std::cout << *easyfind(numbers, 1) << std::endl;
+        std::cout << *easyfind(numbers, 0) << std::endl;
+        std::cout << *easyfind(numbers, -11) << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 }
