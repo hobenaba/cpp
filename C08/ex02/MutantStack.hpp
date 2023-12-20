556c59bab1935fc8cea943ac2ad32ca1f212279c
Houda Benabad <hobenaba1337@gmail.com>
2023-12-20 19:04:46 +0100

    done

---
+++ C08/ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 18:30:31 by hobenaba          #+#    #+#             */
+/*   Updated: 2023/12/20 18:32:13 by hobenaba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <stack>
+
+template <typename T>
+
+class MutantStack : public std::stack<T>
+{
+    
+};
---
+++ C08/ex02/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 18:29:45 by hobenaba          #+#    #+#             */
+/*   Updated: 2023/12/20 18:31:42 by hobenaba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    // MutantStack<int>::iterator it = mstack.begin();
+    // MutantStack<int>::iterator ite = mstack.end();
+    // ++it;
+    // --it;
+    // while (it != ite)
+    // {
+    // std::cout << *it << std::endl;
+    // ++it;
+    // }
+    // std::stack<int> s(mstack);
+    return 0;
+}
