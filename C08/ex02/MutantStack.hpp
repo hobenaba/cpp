91abcb29488f47946df075fca22fbca125946419
Houda Benabad <hobenaba1337@gmail.com>
2023-12-20 19:36:21 +0100

    Doome

--- C08/ex02/MutantStack.hpp
+++ C08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 18:30:31 by hobenaba          #+#    #+#             */
-/*   Updated: 2023/12/20 18:32:13 by hobenaba         ###   ########.fr       */
+/*   Updated: 2023/12/20 19:23:46 by hobenaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,34 @@ template <typename T>
 
 class MutantStack : public std::stack<T>
 {
-    
+    public:
+        MutantStack() {}
+        
+        MutantStack(const MutantStack &src)
+        {
+            *this = src;
+        }
+
+        MutantStack &operator=(const MutantStack &src)
+        {
+            this -> c = src.c ;
+
+            return (*this);
+        }
+        
+        ~MutantStack()
+        {
+            this ->c.clear();
+        }
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        iterator begin()
+        {
+            return (this ->c.begin());
+        }
+        iterator end()
+        {
+            return (this -> c.end());
+        }
 };
--- C08/ex02/main.cpp
+++ C08/ex02/main.cpp
@@ -6,11 +6,12 @@
 /*   By: hobenaba <hobenaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 18:29:45 by hobenaba          #+#    #+#             */
-/*   Updated: 2023/12/20 18:31:42 by hobenaba         ###   ########.fr       */
+/*   Updated: 2023/12/20 19:35:40 by hobenaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
@@ -23,17 +24,39 @@ int main()
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
-    // MutantStack<int>::iterator it = mstack.begin();
-    // MutantStack<int>::iterator ite = mstack.end();
-    // ++it;
-    // --it;
-    // while (it != ite)
-    // {
-    // std::cout << *it << std::endl;
-    // ++it;
-    // }
-    // std::stack<int> s(mstack);
-    return 0;
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+
+    std::cout << "<-----------test with a list--------->" << std::endl;
+
+    std::list <int> test;
+
+    test.push_back(5);
+    test.push_back(17);
+    std::cout << test.back() << std::endl;
+    test.pop_back();
+    std::cout << test.size() << std::endl;
+    test.push_back(3);
+    test.push_back(5);
+    test.push_back(737);
+    test.push_back(0);
+
+    std::list<int>::iterator it1 = test.begin();
+    std::list<int>::iterator ite1 = test.end();
+    ++it1;
+    --it1;
+    while (it1 != ite1)
+    {
+        std::cout << *it1 << std::endl;
+        ++it1;
+    }
 }
